90c7995ecf9c0e4abf4991363805149d23afa1fd
Jonathan <jlemanski1@gmail.com>
2020-03-12 17:59:01 -0600

    Formatted input size & Setup model params

--- imdb_sentiment_analysis.py
+++ imdb_sentiment_analysis.py
@@ -6,6 +6,7 @@ from keras.preprocessing import sequence
 
 VOCAB_SIZE = 5000   # Number of words to load
 INDEX_FROM = 3      # Start at 3 to account for padding/unknown, & start of sentence
+MAX_SEQ_LEN = 128   # Max input length
 
 # Load and assign dataset
 (X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=VOCAB_SIZE, index_from=INDEX_FROM)
@@ -17,5 +18,9 @@ idx_to_word[0] = '<PAD>'
 idx_to_word[1] = '<START>'
 idx_to_word[2] = '<UNK>'
 
+X_train = sequence.pad_sequences(X_train, maxlen=MAX_SEQ_LEN)
+X_test = sequence.pad_sequences(X_test, maxlen=MAX_SEQ_LEN)
 
+# Print result
 print(' '.join([idx_to_word[idx] for idx in X_train[0]]))
+
