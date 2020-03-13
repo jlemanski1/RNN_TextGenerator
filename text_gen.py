2c90cce8f1e784f847b7709f5cb6b867a5561d09
Jonathan <jlemanski1@gmail.com>
2020-03-12 19:11:30 -0600

    Created function to generate sample text from the moel

--- text_gen.py
+++ text_gen.py
@@ -41,3 +41,30 @@ model.add(Activation('softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam')
 
 model.fit(X, y, epochs= 30, batch_size=256)
+
+
+#
+#   Generates and returns sample text from the model
+#
+def sample_from_model(mode, sample_length= 100):
+    seed = randint(0, data_size - sentence_length)
+    # Sentence starter seed
+    seed_sentence = corpus[seed: seed + sentence_length]
+
+    X_pred = np.zeros((1, sentence_length, vocab_size), dtype= np.bool)
+    for t, char in enumerate(seed_sentence):
+        X_pred[0, t, char_to_idx[char]] = 1
+    
+    generated_text = ''
+
+    for i in range(sample_length):
+        prediction = np.argmax(model.predict(X_pred))
+
+        generated_text += idx_to_char[prediction]
+
+        activation = np.zeros((1, 1, vocab_size), dtype= np.bool)
+        activation[0, 0, prediction] = 1
+        X_pred = np.concatenate((X_pred[:, 1:, :], activation), axis= 1)
+    
+    return generated_text
+
